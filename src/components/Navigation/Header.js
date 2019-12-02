af3455fd2d30981347cdcf982c639a165895d17e
kampkelly <kampkellykeys@gmail.com>
2019-12-03 00:04:51 +0100

    create header template

---
+++ src/components/Navigation/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.scss';
+
+export const Header = () => {
+    return (
+        <div>
+            <header>
+                <div className="top-notice">
+                    <span>Use our store locator to find products at a shop near you.</span>
+                </div>
+                <div className="main-nav">
+                    
+                </div>
+            </header>
+        </div>
+    );
+}
