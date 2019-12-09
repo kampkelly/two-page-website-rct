98ffe625876abbc4496e8138d5363cdcaf2d6574
kampkelly <kampkellykeys@gmail.com>
2019-12-09 01:26:46 +0100

    add carousel and navigation lists

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -6,10 +6,28 @@ export const Header = () => {
         <div>
             <header>
                 <div className="top-notice">
-                    <span>Use our store locator to find products at a shop near you.</span>
+                    <span>Use our store locator to find products's at a shop near you.</span>
                 </div>
                 <div className="main-nav">
-                    
+                    <div className="long-nav">
+                        <ul>
+                            <li>BrandName</li>
+                            <li>Home</li>
+                            <li>Products</li>
+                            <li>Health Needs</li>
+                            <li>Ingredients</li>
+                            <li>Our Difference</li>
+                            <li>Shop</li>
+                        </ul>
+                    </div>
+                    <div className="short-nav">
+                        <ul>
+                            <li>My Account</li>
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                        </ul>
+                    </div>
                 </div>
             </header>
         </div>
---
+++ src/components/Slides/MainCarousel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MainCarousel.scss';
+
+export const MainCarousel = () => {
+    return (
+        <div className="carousel-section">
+            <div className="carousel-image"></div>
+        </div>
+    );
+}
